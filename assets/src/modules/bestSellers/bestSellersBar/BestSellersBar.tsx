e67fb9bfefdb5aa220bd8b5bbd883c421b25be10
Adam Kimpton <adam.kimpton.nz@gmail.com>
2022-05-12 15:00:17 +1200

    COMPX341-A3: Make app not compile

--- assets/src/modules/bestSellers/bestSellersBar/BestSellersBar.tsx
+++ assets/src/modules/bestSellers/bestSellersBar/BestSellersBar.tsx
@@ -10,7 +10,8 @@ import pancakes from "../../../images/bestSellers/pancakes.png";
 import pineapple from "../../../images/bestSellers/pineapple.png";
 import umami from "../../../images/bestSellers/umami.png";
 
-const bestSellers = [burgers, italian, noodles, pancakes, pineapple, umami];
+// Oops...
+// const bestSellers = [burgers, italian, noodles, pancakes, pineapple, umami];
 
 export class BestSellersBar extends React.Component {
   render() {
