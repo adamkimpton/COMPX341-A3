99d526c40251882a8611e625be63a5915f121537
Adam Kimpton <adam.kimpton.nz@gmail.com>
2022-05-18 12:49:50 +1200

    COMPX341-A3: Commiting from CI/CD Pipeline

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,11 +1,31 @@
 # Install dependencies via npm.
 echo "[0] > INSTALL"
-npm install
+if npm install; then
+    echo "Install succeeded"
+else 
+    echo "Install failed"
+    exit
+fi
 
 # Build application.
 echo "[1] > BUILD"
-npm run build
+if npm run build; then
+    echo "BUILD succeeded"
+else 
+    echo "BUILD failed"
+    exit
+fi
+
+# Commit & push changes to github.
+echo "[2] > GIT"
+git add -A
+if [ -z "$1" ]; then
+    git commit -m "COMPX341-A3: Commiting from CI/CD Pipeline"
+else
+    git commit -m $1
+fi
+git push
 
 # Start application.
-echo "[2] > START"
+echo "[3] > START"
 npm run start
